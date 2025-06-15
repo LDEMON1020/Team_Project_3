2341857d3c6fff90436e99c0de1a0ee9c00a49de
LDEMON1020 <LD_EMON1020@g.shingu.ac.kr>
2025-06-15 15:00:58 +0900

    [Feat] HP 도움말 생성(Text, 스크린샷 추가 예정)

---
+++ Assets/HPHelp.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPHelp : MonoBehaviour
+{
+    public void GameHPHelp()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("HPMenuHelp1");
+    }
+}
