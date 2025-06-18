2f7ae76bf1ae6719d30586a298a10ca1413a2964
yoogun12 <yoogun1313@gmail.com>
2025-06-18 19:20:26 +0900

    [Feat] 배경음악 조절 슬라이더랑 효과음 조절 슬라이더 구분.

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -1,40 +1,53 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class VolumeSettings : MonoBehaviour
+public class AudioMixerVolume : MonoBehaviour
 {
-    [SerializeField] Slider volumeSlider;
+    public AudioMixer audioMixer;    // 인스펙터에서 오디오믹서 연결
+    public Slider musicSlider;       // 배경음악 슬라이더
+    public Slider sfxSlider;         // 효과음 슬라이더
 
+    private const string MUSIC_PARAM = "MusicVolume";
+    private const string SFX_PARAM = "SFXVolume";
 
     void Start()
     {
-        if(!PlayerPrefs.HasKey("musicVolume"))
-        {
-            PlayerPrefs.SetFloat("musicVolume", 1);
-            Load();
-        }
+        // 저장된 값이 없으면 1로 기본 세팅
+        float musicValue = PlayerPrefs.GetFloat(MUSIC_PARAM, 1f);
+        float sfxValue = PlayerPrefs.GetFloat(SFX_PARAM, 1f);
 
-        else
-        {
-            Load();
-        }
+        musicSlider.value = musicValue;
+        sfxSlider.value = sfxValue;
+
+        SetMusicVolume(musicValue);
+        SetSfxVolume(sfxValue);
+
+        musicSlider.onValueChanged.AddListener(SetMusicVolume);
+        sfxSlider.onValueChanged.AddListener(SetSfxVolume);
     }
 
-    public void ChangeVolume()
+    public void SetMusicVolume(float sliderValue)
     {
-        AudioListener.volume = volumeSlider.value;
-        Save();
+        // 슬라이더 값(0~1)을 데시벨(-80~0)으로 변환
+        float dB = Mathf.Lerp(-80f, 0f, sliderValue);
+        audioMixer.SetFloat(MUSIC_PARAM, dB);
+        PlayerPrefs.SetFloat(MUSIC_PARAM, sliderValue);
     }
 
-    private void Load()
+    public void SetSfxVolume(float sliderValue)
     {
-        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        float dB = Mathf.Lerp(-80f, 0f, sliderValue);
+        audioMixer.SetFloat(SFX_PARAM, dB);
+        PlayerPrefs.SetFloat(SFX_PARAM, sliderValue);
     }
 
-    private void Save()
+    void Awake()
     {
-        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
+        float musicValue = PlayerPrefs.GetFloat("MusicVolume", 1f);
+        float sfxValue = PlayerPrefs.GetFloat("SFXVolume", 1f);
+
+        audioMixer.SetFloat("MusicVolume", Mathf.Lerp(-80f, 0f, musicValue));
+        audioMixer.SetFloat("SFXVolume", Mathf.Lerp(-80f, 0f, sfxValue));
     }
 }
